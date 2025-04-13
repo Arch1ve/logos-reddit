4cf971214e9f6c1360ba97584e1eebcd630b5d7e
Semyon4ik51 <semester13000@gmail.com>
2025-04-13 14:59:55 +0300

    update header

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ function App() {
     <div className="app-container">
       <header className="app-header">
         <div className="header"> 
-          <div className="name-div"><a className="name" href=""> NAME </a></div>
-          <div className="home-div"><a className="home" href=""> HOME </a></div>
-          <div className="friends-div"><a className="friends" href=""> FRIENDS </a></div>
+          <div className="name-div"><a className="name" href="">NAME</a></div>
+          <div className="home-div"><a className="home" href="">HOME</a></div>
+          <div className="friends-div"><a className="friends" href="">FRIENDS</a></div>
           <div className="picture-div"><img className="picture" src="https://sun9-21.userapi.com/s/v1/ig2/zSx-VG34YWZVNGjOtMIiKgpISGSWsWKazGblqgA_PsO8oQTSIFkZ4oGl6CJeu7lvfHnfJa4PWvcr1f0m4NRCaoXy.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,1024x1024&from=bu&u=NFUR0_qJPBI4HxKLaoo3Ria_2MjxUgfHxL_m4yOiFU4&cs=807x807" alt="sova"></img></div>
-          <div className="log-in-div"><a className="log-in" href=""> Login </a></div>
+          <div className="log-in-div"><a className="log-in" href="">Login</a></div>
         </div>
         
       </header>
