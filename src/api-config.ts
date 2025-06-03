c1c17075b8c96d098db53ca4b48c7e5eec6e0da3
Arch1ve <afanasiy.m2002@gmail.com>
2025-06-03 10:20:07 +0300

    fix api path

--- src/api-config.ts
+++ src/api-config.ts
@@ -1,2 +1,2 @@
-// export const API_URL = "http://logos-space.ru/api" as const
-export const API_URL = "http://localhost:3000/api" as const
+export const API_URL = "http://logos-space.ru/api" as const
+// export const API_URL = "http://localhost:3000/api" as const
